17e06251f3a07f0031f6916875fe391a4f62d514
squid <luisquid.dev@gmail.com>
2023-02-19 21:56:34 -0500

    Module 4 finished

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,15 +1,23 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+
   const expenses = [
     {title:'Car Loan', amount: 2050000, date: new Date(2022, 7, 20)},
     {title:'Credit Card', amount: 55000, date: new Date(2022, 7, 22)},
     {title:'Rappi Dinner Sushi', amount: 70000, date: new Date(2022, 7, 22)},
     {title:'Game', amount: 40000, date: new Date(2022, 7, 23)}
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("IN APP.JS");
+    console.log(expense);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses data = {expenses}/>
     </div>
   );
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,16 +1,31 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+import React, {useState} from "react";
 
 function Expenses (props)
 {
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const FilterDataByDate = (date) => {
+        console.log(date);
+        setFilteredYear(date);
+    };
+
     return(
-        <Card className="expenses">
+        <div>
+            <ExpensesFilter selected = {filteredYear} onFilterDateSet = {FilterDataByDate}/>
+            <Card className="expenses">
             <ExpenseItem title = {props.data[0].title} amount = {props.data[0].amount} date = {props.data[0].date} />
             <ExpenseItem title = {props.data[1].title} amount = {props.data[1].amount} date = {props.data[1].date}/>
             <ExpenseItem title = {props.data[2].title} amount = {props.data[2].amount} date = {props.data[2].date}/>
             <ExpenseItem title = {props.data[3].title} amount = {props.data[3].amount} date = {props.data[3].date}/>
         </Card>
+        </div>
+        
     );
 }
 
